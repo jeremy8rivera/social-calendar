ef0bed2818179ae88f21a38cc0026b70ddc769a6
jeremy8rivera <jeremy8rivera@gmail.com>
2019-04-24 02:21:53 -0400

    Dashboard update: need a seperate branch

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,7 +1,18 @@
 <template>
+
 <div>
-  <Header></Header>
+    <Header></Header>
     <div>Welcome {{ this.$root.$data.username }}</div>
+    <div id="calendarSelector">
+        <div id="personalCalendar" class="calendarChoice">Personal</div>
+        <div id="groupCalendar" class="calendarChoice">Groups</div>
+    </div>
+    <div id="eventList" class="test">
+        <h2>Events</h2>
+    </div>
+    <div id="visualCalendar" class="test">
+        <h2>Calendar</h2>
+    </div>
 
 </div>
 </template>
@@ -20,3 +31,47 @@ export default {
 }
 
 </script>
+
+<style>
+#calendarSelector{
+    font-size: 24px;
+    text-align: center;
+}
+
+#groupCalendar{
+    border-right: 1px solid black;
+}
+
+.calendarChoice{
+    margin: 0 auto;
+    display: inline;
+    border-width: 1px 0px 1px 1px;
+    border-color: black;
+    border-style: solid;
+    padding: 10px 20px 10px 20px;
+}
+
+#eventList{
+    width: 19%;
+    height: 100px;
+    background-color: gray;
+    float: left;
+}
+
+h2{
+    text-align: center;
+}
+
+#visualCalendar{
+    width:80%;
+    height: 100px;
+    background-color: orange;
+    float: left;
+}
+
+.test{
+    border: 1px solid black;
+    margin-top: 20px;
+}
+
+</style>
