f36a61d7cba2a4cb082d1a2960a2540ef1dad92e
Jay <jay@10-18-104-226.dynapool.wireless.nyu.edu>
2019-05-01 10:17:05 -0400

    connected settings page to backend

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -41,7 +41,6 @@ export default {
         console.error(error)
       })
 
-
     }
   },
   beforeMount () {
--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -6,6 +6,7 @@
         <input type="password" v-model="password" placeholder="Enter Current Password" />
         <input type="password" v-model="npassword1" placeholder="New Password" />
         <input type="password" v-model="npassword2" placeholder="Re-enter New Password" />
+        <br>
         <button type="button" v-on:click="updatePassword()">Update</button>
 
     </div>
@@ -15,22 +16,38 @@
 <script>
 
 import Header from '../components/Header'
+import axios from 'axios'
+import sha1 from 'sha1'
 
 export default {
     name: 'Settings',
     methods: {
         updatePassword() {
-            if (this.password !== this.$root.$data.tempAccount.password) {
-                window.alert("Current password is incorrect")
-            } else if (this.npassword1 !== this.npassword2) {
+
+            if (this.npassword1 !== this.npassword2) {
                 window.alert("New passwords do not match")
             } else {
-                window.alert("Successfully updated password")
+                axios({ method: 'GET', 'url': this.$root.$data.backendAddress + '/settings/' + this.$root.$data.username.toLowerCase() + '/' 
+                + sha1(this.password) + '/' + sha1(this.npassword1) })
+                .then(result => {
+                    console.log(result)
+                    if (result.data.successful) {
+                        window.alert('Successfully changed your password')
+                        this.$router.push('/dashboard')
+                    } else {
+                        window.alert("Password wrong")
+                    }
+                })
             }
         }
     },
     components: {
         Header
+    }, 
+    beforeMount () {
+      if (!this.$root.$data.authenticated) {
+          this.$router.push('/login')
+      }
     }
 }
 
