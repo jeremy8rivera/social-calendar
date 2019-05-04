354d07aedd5efeb6fc1e3233dbf622bc9b568be7
Zafir Hasan <zafirhsn@gmail.com>
2019-05-04 01:30:15 -0400

    Basic login styling

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    <input type="text" v-model="username" placeholder="Username" />
-    <input type="password" v-model="password" placeholder="Password" />
-    <button type="button" v-on:click="login()">Login</button>
-    <br>
-    <router-link to='signup'>Create account</router-link>
+
+<body>
+
+  <div class="login">
+    <p>Username</p>
+    <input type="text" v-model="username" placeholder="Username" required>
+    <!-- <input type="email" name="email" placeholder="Email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"> -->
+    <input type="password" v-model="password" placeholder="Password" required minlength="3">
+    <p>The password must be > 4 char</p>
+    <!-- You also can use only one input and use "pointer-events: none;", but you still can select it using tab -->
+    
+    <input type="submit" value="Login" disabled>
+    <input type="submit" value="Login" v-on:click="login()">
+    <router-link to='signup' tag="rl" >Create account</router-link>
   </div>
+
+</body>
+
 </template>
+
+
+
+<style>
+    @import url("https://fonts.googleapis.com/css?family=Roboto:100,300,400,500");
+</style>
+
+<!--This is how you add css to Vue -->
+<style scoped src="../css/login.css"></style>
 
 <script>
 
@@ -33,10 +53,6 @@ export default {
            this.$root.$data.username = result.data.data.Item.email
            this.$root.$data.name = result.data.data.Item.name
 
-          axios( { method: 'GET', 'url': this.$root.$data.backendAddress + '/loadevents/' + this.$root.$data.username } )
-          .then(result => {
-          this.$root.$data.events = result.data.events
-          })
            this.$router.push('/dashboard')
          } else {
            window.alert('Login failed. Please try again')
